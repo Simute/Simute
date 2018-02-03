91b2833166f04cf04af4394317065891c1591c1f
Your Name <you@example.com>
2018-02-03 15:59:00 +0200

    cleanup

---
+++ app/src/main/java/com/simona/todo/to_doapp/model/ItemVO.java
@@ -0,0 +1,11 @@
+package com.simona.todo.to_doapp.model;
+
+/**
+ * Created by Simonos on 2/3/2018.
+ */
+
+public class ItemVO {
+    public int id;
+    public int done;
+    public String title;
+}
